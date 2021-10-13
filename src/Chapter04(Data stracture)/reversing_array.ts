ef4f545f2f0815feda10831ac757f3e892ba3029
Vkube <sawmati@gmail.com>
2021-10-13 19:38:53 +0300

    add Chapter04

---
+++ src/Chapter04(Data stracture)/reversing_array.ts
@@ -0,0 +1,23 @@
+//Напишите две функции reverseArray и reverseArrayInPlace
+// Первая функция reverseArray принимает массив в качестве аргумента и создает новый массив, содержащий те же элементы в обратном порядке.
+//Вторая reverseArrayInPlace делает то же самое что и метод reverse.
+
+const reverseArray = (array) => {
+  let newArr = [];
+
+  for (let i = array.length - 1; i < 0; i--) {
+    newArr.push(i);
+  }
+  return newArr;
+};
+
+const reverseArrayInPlace = ()=>{
+
+
+
+
+}
+
+
+
+console.log(reverseArray(["A", "B", "C"]));
