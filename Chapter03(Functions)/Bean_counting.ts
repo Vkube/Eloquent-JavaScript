36a5809a0b622eb27aafb1cbf8500d54c5b58c4f
Vkube <sawmati@gmail.com>
2021-10-13 15:05:34 +0300

    add Chapter03

---
+++ Chapter03(Functions)/Bean_counting.ts
@@ -0,0 +1,14 @@
+function countChar(word: string, letter: string): number {
+  let count = 0;
+  for (let i = 0; i < word.length; i++) {
+    if (word.charAt(i) == letter) {
+      count++;
+    }
+  }
+  return count;
+}
+function countBs(string) {
+  return countChar(string, "B");
+}
+let result = countBs('Bdcd');
+console.log(result);
